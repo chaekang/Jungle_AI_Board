48c636f928d5cada087328d75f7daaa28e281f29
chaekang <herry612@naver.com>
2026-06-10 15:47:25 +0900

    chore: my-app 삭제

--- my-app/src/pages/NotFoundPage.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function NotFoundPage() {
-  return (
-    <div>
-      <h1>page를 찾을 수 없습니다</h1>
-    </div>
-  )
-}
--- my-app/src/pages/UserPage.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function UserPage() {
-  return (
-    <div>
-      <h1>여기는 userpage 입니다</h1>
-    </div>
-  )
-}
